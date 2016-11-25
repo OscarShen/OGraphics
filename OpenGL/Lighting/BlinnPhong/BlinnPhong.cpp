fcfc9542aeeb235abbdc75abd4cd571895748d64
oscar <oscarshen09@foxmail.com>
2016-11-25 21:32:46 +0800

    blinn phong

---
+++ OpenGL/Lighting/BlinnPhong/BlinnPhong.cpp
@@ -0,0 +1,188 @@
+#include <string>
+#include <iostream>
+#include <array>
+#include <vector>
+#include <memory>
+
+#include <GL45\glew.h>
+#include <GLFW\glfw3.h>
+
+#include "..\..\Camera\Camera.h"
+#include "..\..\Shader\Shader.h"
+#include "..\..\Texture\Texture.h"
+#include "..\..\ErrorCheck\ErrorCheck.h"
+
+#include <glm\glm.hpp>
+#include <glm\gtc\matrix_transform.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
+// Properties
+GLuint screenWidth = 800, screenHeight = 600;
+
+// Function prototypes
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void do_Movement();
+
+Camera camera;
+
+bool keys[1024];
+GLfloat lastX = 400, lastY = 300;
+bool firstMouse = true;
+
+GLfloat deltaTime = 0.0f;
+GLfloat lastFrame = 0.0f;
+
+bool isBlinn = false;
+
+int main() {
+	// Init GLFW
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+
+	// multisample
+	// glfwWindowHint(GLFW_SAMPLES, 4);
+
+	GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight,
+		"ILoveOpenGL", nullptr, nullptr); // Windowed
+	glfwMakeContextCurrent(window);
+
+	// Set the required callback functions
+	glfwSetKeyCallback(window, key_callback);
+	glfwSetCursorPosCallback(window, mouse_callback);
+
+	// Options
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+	// Initialize GLEW to setup the OpenGL Function pointers
+	glewExperimental = GL_TRUE;
+	glewInit();
+
+	// Define the viewport dimensions
+	glViewport(0, 0, screenWidth, screenHeight);
+
+#pragma region "object_initialization"
+	GLfloat planeVertices[] = {
+		// Positions          // Normals         // Texture Coords
+		8.0f, -0.5f,  8.0f,  0.0f, 1.0f, 0.0f,  5.0f, 0.0f,
+		-8.0f, -0.5f,  8.0f,  0.0f, 1.0f, 0.0f,  0.0f, 0.0f,
+		-8.0f, -0.5f, -8.0f,  0.0f, 1.0f, 0.0f,  0.0f, 5.0f,
+
+		8.0f, -0.5f,  8.0f,  0.0f, 1.0f, 0.0f,  5.0f, 0.0f,
+		-8.0f, -0.5f, -8.0f,  0.0f, 1.0f, 0.0f,  0.0f, 5.0f,
+		8.0f, -0.5f, -8.0f,  0.0f, 1.0f, 0.0f,  5.0f, 5.0f
+	};
+	GLuint planeVAO, planeVBO;
+	glGenBuffers(1, &planeVBO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+	glGenVertexArrays(1, &planeVAO);
+	glBindVertexArray(planeVAO);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), 0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(3 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GL_FLOAT), (GLvoid*)(6 * sizeof(GL_FLOAT)));
+	glEnableVertexAttribArray(2);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// Light source
+	glm::vec3 lightPos(0.0f, 0.0f, 0.0f);
+
+	GLuint floorTexture = loadTexture("resource/texture/wood.png");
+	Shader shader("Lighting/BlinnPhong/blinn_phong.vs", "Lighting/BlinnPhong/blinn_phong.frag");
+
+	glEnable(GL_DEPTH_TEST);
+#pragma endregion
+	int count = 0;
+	while (!glfwWindowShouldClose(window)) {
+		// Set frame time
+		GLfloat currentFrame = (GLfloat)glfwGetTime();
+		deltaTime = currentFrame - lastFrame;
+		lastFrame = currentFrame;
+		if (++count % 120 == 0)
+			std::cout << static_cast<int>(1 / deltaTime) << std::endl;
+
+		// Clear the colorbuffer
+		glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+#pragma region "render"
+		shader.use();
+		glm::mat4 view = camera.getViewMatrix();
+		glm::mat4 projection = glm::perspective(45.0f, (GLfloat)screenWidth / screenHeight, 0.1f, 100.0f);
+		glUniformMatrix4fv(glGetUniformLocation(shader.program, "view"), 1, GL_FALSE, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shader.program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
+		// Set light uniforms
+		glUniform3fv(glGetUniformLocation(shader.program, "lightPos"), 1, &lightPos[0]);
+		glUniform3fv(glGetUniformLocation(shader.program, "viewPos"), 1, &camera.position[0]);
+		glUniform1i(glGetUniformLocation(shader.program, "blinn"), isBlinn);
+glCheckError();
+		glBindVertexArray(planeVAO);
+		glBindTexture(GL_TEXTURE_2D, floorTexture);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		glBindVertexArray(0);
+
+#pragma endregion
+		// Check and call events
+		glfwPollEvents();
+		do_Movement();
+
+		// Swap the buffers
+		glfwSwapBuffers(window);
+	}
+#pragma region "free"
+
+#pragma endregion
+	glfwTerminate();
+	return 0;
+}
+
+#pragma region "User input"
+// Moves/alters the camera positions based on user input
+void do_Movement()
+{
+	// Camera controls
+	if (keys[GLFW_KEY_W])
+		camera.do_key(CameraMovement::MOVEFORWARD, deltaTime);
+	if (keys[GLFW_KEY_S])
+		camera.do_key(CameraMovement::MOVEBACKWARD, deltaTime);
+	if (keys[GLFW_KEY_A])
+		camera.do_key(CameraMovement::MOVELEFT, deltaTime);
+	if (keys[GLFW_KEY_D])
+		camera.do_key(CameraMovement::MOVERIGHT, deltaTime);
+}
+
+// Is called whenever a key is pressed/released via GLFW
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (action == GLFW_PRESS)
+		keys[key] = true;
+	else if (action == GLFW_RELEASE)
+		keys[key] = false;
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	if (firstMouse)
+	{
+		lastX = (GLfloat)xpos;
+		lastY = (GLfloat)ypos;
+		firstMouse = false;
+	}
+
+	GLfloat xoffset = (GLfloat)(xpos - lastX);
+	GLfloat yoffset = (GLfloat)(lastY - ypos);
+
+	lastX = (GLfloat)xpos;
+	lastY = (GLfloat)ypos;
+
+	camera.do_mouse(xoffset, yoffset);
+}
+#pragma endregion
---
+++ OpenGL/Lighting/BlinnPhong/blinn_phong.frag
@@ -0,0 +1,37 @@
+#version 330 core
+
+in VS_OUT {
+	vec3 FragPos;
+	vec3 Normal;
+	vec2 TexCoords;
+} fs_in;
+
+uniform vec3 lightPos;
+uniform vec3 viewPos;
+uniform bool blinn;
+uniform sampler2D floorTexture;
+
+out vec4 FragColor;
+
+void main() {
+	vec3 color = texture(floorTexture, fs_in.TexCoords).rgb;
+	// Ambient
+	vec3 ambient = color * 0.05f;
+	// Diffuse
+	vec3 lightDir = normalize(lightPos - fs_in.FragPos);
+	vec3 normal = normalize(fs_in.Normal);
+	float diff = max(dot(lightDir, normal), 0.0f);
+	vec3 diffuse = diff * color;
+	// Specular
+	vec3 viewDir = normalize(viewPos - fs_in.FragPos);
+	float spec = 0.0f;
+
+	//vec3 reflectDir = reflect(-lightDir, normal);
+	//spec = pow(max(dot(viewDir, reflectDir), 0.0f), 32.0f);
+
+	vec3 halfwayDir = normalize(viewDir + lightDir);
+	spec = pow(dot(normal, halfwayDir), 32.0f);
+	vec3 specular = vec3(0.3f) * spec;
+
+	FragColor = vec4(ambient + diffuse + specular, 1.0f);
+}
